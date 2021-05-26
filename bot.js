d1206f708c4ccf4fc10a027c5b63d745ca939dd3
Said Neder <saidneder1@gmail.com>
2021-05-26 10:42:16 -0500

    Removing Calendar and links

--- bot.js
+++ bot.js
@@ -42,34 +42,11 @@ function help() {
     9mail =
     It will reply with a gmail's link📫
 
-    9calendar =
-    It will reply with a calendar's link📅
-
     9idukay =
-    It will reply with a Idukay Link!
+    It will reply with a Idukay Link!📝
 
-    9{class} =
-     it will show the class link📝
-
-             📚classes code📚
-      · 9mf -> Matematica y Fisica📘
-
-      · 9hi -> Historia📒
-
-      · 9fi -> Filosofia📒
-
-      · 9in -> Ingles📘
-
-      · 9pe -> Participacion Estudiantil📓
-
-      · 9qu -> Quimica📗
-
-      · 9bi -> Biologia📗
-
-      · 9le -> Lenguaje📖
-
-     9info =
-     Information about the project!😄
+    9info =
+    Information about the project!🚀
   `;
 }
 
@@ -162,15 +139,16 @@ client.on("message", (message) => {
 `;
   var days = [monday, tuesday, wednesday, thursday, friday];
   const date = new Date();
-  const today = date.getUTCDay();
-  const calendar = "https://calendar.google.com/calendar/u/1/r";
+  const today = date.getDay();
+  const hour = date.getHours().toString();
+  const minutes = date.getMinutes().toString();
+  const time = hour + minutes;
   const gmail = "https://mail.google.com/mail/u/1/?ogbl#inbox";
   const idukay = "https://idukay.net/#/";
   const github = "https://github.com/Cattodeveloper909";
   const discordid = "DarthNeder#2142";
   // Print every message in the server in the console
-  console.log(message.content);
-
+  console.log(message.content, time);
   // if one of the message is help do this:
   if (message.content === `${prefix}help`) {
     // reply in the same channel from it was sent
@@ -183,22 +161,6 @@ client.on("message", (message) => {
     message.channel.send(`Voila! ${idukay}`);
   } else if (message.content === `${prefix}mail`) {
     message.channel.send(`Sent! ${gmail}`);
-  } else if (message.content === `${prefix}mf`) {
-    message.channel.send(`Math class: ${fima}`);
-  } else if (message.content === `${prefix}hi`) {
-    message.channel.send(`History class: ${historia}`);
-  } else if (message.content === `${prefix}fi`) {
-    message.channel.send(`Philosophy class: ${filosofia}`);
-  } else if (message.content === `${prefix}qu`) {
-    message.channel.send(`Chemistry class: ${quimica}`);
-  } else if (message.content === `${prefix}bi`) {
-    message.channel.send(`Biology class: ${biologia}`);
-  } else if (message.content === `${prefix}le`) {
-    message.channel.send(`Spanish class: ${lenguaje}`);
-  } else if (message.content === `${prefix}in`) {
-    message.channel.send(`English class: ${ingles}`);
-  } else if (message.content === `${prefix}pe`) {
-    message.channel.send(`PPE class: ${ppe}`);
   } else if (message.content === `${prefix}day`) {
     message.channel.send(`Today's scheadule: ${days[today + 1]} `);
   } else if (message.content === `${prefix}info`) {
