82f65ce2e321a9b99b253f75d8e730087364e0a6
Said Neder <saidneder1@gmail.com>
2021-06-02 19:01:23 -0500

    commiting error on purpose to make heroku offline

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const cron = require("cron");
 const client = new Discord.Client();
 
 // remote config file where my data is stored
-const { prefix, token } = require("./config.json");
+const { prefix, token, channelid } = require("./config.json");
 
 // Date library of node
 
@@ -130,18 +130,11 @@ client.on("message", (message) => {
 
 `;
   var days = [monday, tuesday, wednesday, thursday, friday];
-  const date = new Date();
-  const today = date.getDay();
-  const seconds = date.getSeconds().toString();
-  const hour = date.getHours().toString();
-  const minutes = date.getMinutes().toString();
-  const time = hour + minutes + seconds;
   const gmail = "https://mail.google.com/mail/u/1/?ogbl#inbox";
   const idukay = "https://idukay.net/#/";
   const github = "https://github.com/Cattodeveloper909";
   const discordid = "DarthNeder#2142";
-  // Print every message in the server in the console
-  console.log(message.content, time);
+
   // if one of the message is help do this:
   if (message.content === `${prefix}help`) {
     // reply in the same channel from it was sent
@@ -155,79 +148,85 @@ client.on("message", (message) => {
   } else if (message.content === `${prefix}mail`) {
     message.channel.send(`Sent! ${gmail}`);
   } else if (message.content === `${prefix}day`) {
-    message.channel.send(`Today's scheadule: ${days[today + 1]} `);
+    message.channel.send(`Today's scheadule: ${days[today - 1]} `);
   } else if (message.content === `${prefix}info`) {
     message.channel.send(
       `This was made by Said Neder\nGithub: ${github}\nDiscord: ${discordid}`
     );
   }
-
-  function notificationclass() {
-    return `
-    You have class! GO NOW ${idukay}
-    `;
-  }
-
-  function classbreak() {
-    return `
-    You have a break right now, patience and be calm, enjoy!
-    `;
-  }
-  // send a message if it is the time specified.
-  let classnotification1 = new cron.CronJob("00 00 3 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification2 = new cron.CronJob("00 35 3 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification3 = new cron.CronJob("00 00 3 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification4 = new cron.CronJob("00 10 4 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification5 = new cron.CronJob("00 45 4 * * *", () => {
-    channel.send(classbreak);
-  });
-
-  let classnotification6 = new cron.CronJob("00 00 5 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification7 = new cron.CronJob("00 35 5 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification8 = new cron.CronJob("00 10 6 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification9 = new cron.CronJob("00 45 6 * * *", () => {
-    channel.send(classbreak);
-  });
-
-  let classnotification10 = new cron.CronJob("00 00 7 * * *", () => {
-    channel.send(notificationclass);
-  });
-
-  let classnotification11 = new cron.CronJob("00 35 7 * * *", () => {
-    channel.send(`You have ended your classes! Enjoy the rest of the day!`);
-  });
-
-  // When you want to start it, use:
-  classnotification1.start();
-  classnotification2.start();
-  classnotification3.start();
-  classnotification4.start();
-  classnotification5.start();
-  classnotification6.start();
-  classnotification7.start();
-  classnotification8.start();
-  classnotification9.start();
-  classnotification10.start();
-  classnotification11.start();
 });
+
+function notificationclass() {
+  return `
+  You have class! GO NOW ${idukay}
+  `;
+}
+
+function classbreak() {
+  return `
+  You have a break right now, patience and be calm, enjoy!
+  `;
+}
+
+//const kalamardo = client.guilds.cache.get("814143626647961640");
+//console.log(kalamardo);
+//const channell = kalamardo.channels.cache.get("814147482719748146");
+// send a message if it is the time specified.
+let classnotification1 = new cron.CronJob("00 00 3 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification2 = new cron.CronJob("00 35 3 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification3 = new cron.CronJob("00 00 3 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification4 = new cron.CronJob("00 10 4 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification5 = new cron.CronJob("00 45 4 * * *", () => {
+  channel.send(classbreak);
+});
+
+let classnotification6 = new cron.CronJob("00 00 5 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification7 = new cron.CronJob("00 35 5 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification8 = new cron.CronJob("00 10 6 * * *", () => {
+  channel.send(notificationclass);
+});
+
+let classnotification9 = new cron.CronJob("00 45 6 * * *", () => {
+  channel.send(classbreak);
+});
+
+let classnotification10 = new cron.CronJob("00 00 7 * * *", () => {
+  message.channel.send(notificationclass);
+});
+
+let classnotification11 = new cron.CronJob("00 38 18 * * *", () => {
+  channel.send(`You have ended your classes! Enjoy the rest of the day!`);
+});
+
+// When you want to start it, use:
+classnotification1.start();
+classnotification2.start();
+classnotification3.start();
+classnotification4.start();
+classnotification5.start();
+classnotification6.start();
+classnotification7.start();
+classnotification8.start();
+classnotification9.start();
+classnotification10.start();
+classnotification11.start();
+
+miauuuuuu;
